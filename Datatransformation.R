776a60341929a9a3fe018510043f1d144cd33cbf
Mahmoud Abdelwahab Elshiekh <38630439+Mahelsheikh@users.noreply.github.com>
2018-10-19 17:46:02 +0300

    Update Datatransformation.R

--- Datatransformation.R
+++ Datatransformation.R
@@ -1,12 +1,17 @@
 library(dplyr)
 library(tidyr)
+## Downloading the file
 fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileurl,destfile = "helec.zip")
 downloadtime<-date()
+## unziped the file
 unzip("./helec.zip")
+## read table
 helec<-read.table("./household_power_consumption.txt", sep = ";", header = TRUE)
+## filtered the required period
 selected<-c("1/2/2007","2/2/2007")
 feb<-helec[helec$Date %in% selected,]
+## Data transformation to dates and numeric values as suitable for each column and variable type
 feb$Date<-as.Date(feb$Date, format = "%d/%m/%Y")
 feb<-feb %>% unite (col= "datetime",Date,Time, sep = " ")
 feb$datetime<-strptime(feb$datetime, format = "%Y-%m-%d %H:%M:%S")
